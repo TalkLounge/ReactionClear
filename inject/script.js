b0821ac4421bb5d6883f7637f243540d08eaa42a
TalkLounge <talklounge@yahoo.de>
2020-02-07 21:07:53 +0100

    Fixed inject bugs, changed data structure & overworked popup

--- inject/script.js
+++ inject/script.js
@@ -1,42 +1,83 @@
-browser.storage.sync.get(["searchTerms"], function(data) {
-	if (typeof(data) === "undefined" || typeof(data["searchTerms"]) === "undefined")  {
-		//return;
-	}
-	var searchTerms = [{"block": {"title": "Reaktion"}}, {"block": {"title": "reagiert"}}, {"block": {"title": "reagiere"}}, {"block": {"title": "reaction"}}, {"block": {"title": "reacts"}}, {"block": {"title": "ungeklickt"}}, {"block": {"channel": "Hochformat"}}, {"block": {"channel": "ConCrafter"}}, {"block": {"channel": "JAUSE"}}, {"block": {"channel": "KuchenTV Uncut"}}];//data["searchTerms"];
-	
-	/*
-	block.channel & block.title = exclude.title
-	block.title = exclude.channel | exclude.title
-	block.channel = exclude.title
-	*/
-	
-	function sleep(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms));
-	}
-	
-	var running = {"youtubeStartpage": false, "youtubeRecommended": false};
-	var count = {"youtubeStartpage": 1, "youtubeRecommended": 1};
-	
-	async function youtubeClear(elemList, title1, title2, channel1, channel2, elem1, elem2, countName) {
-		if (running[countName]) {
+var searchTerms = [
+	{"description": "Reactions", "block": [
+		{"title": "Reaktion"},
+		{"title": "reagiert"},
+		{"title": "reagiere"},
+		{"title": "reaction"},
+		{"title": "reacts"},
+		{"title": "ungeklickt"},
+		{"channel": "KuchenTV Uncut"}
+	], "exclude": [
+		{"title": "Reved"}
+	]}, {"block": [
+		{"channel": "ConCrafter"}
+	]}, {"block": [
+		{"channel": "Hochformat"}
+	]}, {"block": [
+		{"channel": "JAUSE"}
+	]}
+];
+
+function getSearchTerms() {
+	browser.storage.sync.get(["searchTerms"], function(data) {
+		if (typeof(data) === "undefined" || typeof(data["searchTerms"]) === "undefined")  {
 			return;
+		} else {
+			searchTerms = data["searchTerms"];
 		}
-		running[countName] = true;
-		elemList = $(elemList).length;
-		for (var i = count[countName]; i <= elemList; i++) {
-			await sleep(50);
-			var titleCc = $(title1 + i + title2).html();
-			var channelCc = $(channel1 + i + channel2).html();
-			if (titleCc && channelCc && $(elem1 + i + elem2).css("display") !== "none") {
-				titleCc = titleCc.trim();
-				channelCc = channelCc.trim();
-				var title = titleCc.toLowerCase();
-				var channel = channelCc.toLowerCase();
-				var found = false;
-				for (var j = 0; j < searchTerms.length; j++) {
-					var searchTerm = searchTerms[j];
-					var block = searchTerm["block"] || {};
-					var exclude = searchTerm["exclude"] || {};
+	});
+}
+
+getSearchTerms();
+
+/*
+block.channel & block.title = exclude.title
+block.title = exclude.channel | exclude.title
+block.channel = exclude.title
+*/
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var running = {"youtubeStartpage": false, "youtubeRecommended": false};
+var count = {"youtubeStartpage": 1, "youtubeRecommended": 1};
+
+async function youtubeClear(elemList, title1, title2, channel1, channel2, elem1, elem2, countName, sleepMs) {
+	if (running[countName]) {
+		return;
+	}
+	if (countName === "youtubeStartpage") {
+		console.log("youtubeClear called");
+	}
+	running[countName] = true;
+	elemList = $(elemList).length;
+	var bla = false;
+	for (var i = count[countName]; i <= elemList; i++) {
+		await sleep(sleepMs);
+		var titleCc = $(title1 + i + title2).html();
+		var channelCc = $(channel1 + i + channel2).html();
+		if (titleCc && channelCc && $(elem1 + i + elem2).css("display") !== "none") {
+			if (! bla) {
+				bla = true;
+				console.log("title found");
+			}
+			titleCc = titleCc.trim();
+			channelCc = channelCc.trim();
+			var title = titleCc.toLowerCase();
+			var channel = channelCc.toLowerCase();
+			var found = false;
+			searchTermsLoop:
+			for (var j = 0; j < searchTerms.length; j++) {
+				if (j === 0) {
+					console.log("searchTermsLoop");
+				}
+				var exclude = searchTerms[j]["exclude"] || {};
+				for (var l = 0; l < (searchTerms[j]["block"] || {}).length; l++) {
+					if (j === 0 && l === 0) {
+						console.log("blockLoop");
+					}
+					var block = searchTerms[j]["block"][l];
 					if (block["channel"] && block["channel"].toLowerCase() === channel && block["title"] && title.indexOf(block["title"].toLowerCase()) !== -1) {
 						found = true;
 						for (var k = 0; k < exclude.length; k++) {
@@ -65,58 +106,84 @@ browser.storage.sync.get(["searchTerms"], function(data) {
 					if (found) {
 						$(elem1 + i + elem2).css("display", "none");
 						console.log('[Addon] ReactionClear: Removed "'+ titleCc +'" from "'+ channelCc +'"');
-						break;
+						break searchTermsLoop;
 					}
 				}
-			} else if (! titleCc || ! channelCc) {
-				count[countName] = i;
-				running[countName] = false;
-				return;
 			}
-		}
-		running[countName] = false;
-	}
-	
-	function youtubeStartpage() {
-		youtubeClear('.style-scope.ytd-rich-grid-renderer', 'ytd-rich-item-renderer.ytd-rich-grid-renderer:nth-child(', ') > div:nth-child(1) > ytd-rich-grid-video-renderer:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > h3:nth-child(1) > a:nth-child(2) > yt-formatted-string:nth-child(1)', 'ytd-rich-item-renderer.ytd-rich-grid-renderer:nth-child(', ') > div:nth-child(1) > ytd-rich-grid-video-renderer:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > ytd-video-meta-block:nth-child(2) > div:nth-child(1) > div:nth-child(1) > ytd-channel-name:nth-child(1) > div:nth-child(1) > div:nth-child(1) > yt-formatted-string:nth-child(1) > a:nth-child(1)', 'ytd-rich-item-renderer.ytd-rich-grid-renderer:nth-child(', ')', "youtubeStartpage");
-	}
-	
-	function youtubeRecommended() {
-		youtubeClear('.style-scope.ytd-compact-video-renderer', 'ytd-compact-video-renderer.style-scope:nth-child(', ') > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > h3:nth-child(1) > span:nth-child(2)', 'ytd-compact-video-renderer.style-scope:nth-child(', ') > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(2) > ytd-video-meta-block:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ytd-channel-name:nth-child(1) > div:nth-child(1) > div:nth-child(1) > yt-formatted-string:nth-child(1)', 'ytd-compact-video-renderer.style-scope:nth-child(', ') > div:nth-child(1)', "youtubeRecommended");
-	}
-	
-	function youtubeClearAll() {
-		youtubeStartpage();
-		youtubeRecommended();
-	}
-	
-	setTimeout(youtubeClearAll, 1000);
-	
-	var runningScroll = false;
-	var oldScroll = $(document).scrollTop();
-	$(document).on("scroll", function() {
-		var newScroll = $(document).scrollTop();
-		if (newScroll > oldScroll && ! runningScroll) {
-			oldScroll = newScroll;
-			runningScroll = true;
-			setTimeout(youtubeClearAll, 1000);
-			setTimeout(function() {
-				runningScroll = false;
-			}, 500);
-		}
-	});
-	
-	var oldURL = document.URL;
-	function urlChanged() {
-		var newURL = document.URL;
-		if (newURL !== oldURL) {
-			oldURL = newURL;
-			oldScroll = $(document).scrollTop();
-			count["youtubeStartpage"] = 1;
-			count["youtubeRecommended"] = 1;
-			setTimeout(youtubeClearAll, 1000);
+		} else if (! titleCc || ! channelCc) {
+			if (! bla) {
+				bla = true;
+				console.log("title not found");
+			}
+			count[countName] = i;
+			running[countName] = false;
+			return;
 		}
 	}
-	
-	setInterval(urlChanged, 1000);
+	running[countName] = false;
+}
+
+function youtubeStartpage() {
+	youtubeClear('.style-scope.ytd-rich-grid-renderer', 'ytd-rich-item-renderer.ytd-rich-grid-renderer:nth-child(', ') > div:nth-child(1) > ytd-rich-grid-video-renderer:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > h3:nth-child(1) > a:nth-child(2) > yt-formatted-string:nth-child(1)', 'ytd-rich-item-renderer.ytd-rich-grid-renderer:nth-child(', ') > div:nth-child(1) > ytd-rich-grid-video-renderer:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > ytd-video-meta-block:nth-child(2) > div:nth-child(1) > div:nth-child(1) > ytd-channel-name:nth-child(1) > div:nth-child(1) > div:nth-child(1) > yt-formatted-string:nth-child(1) > a:nth-child(1)', 'ytd-rich-item-renderer.ytd-rich-grid-renderer:nth-child(', ')', "youtubeStartpage", 50);
+}
+
+function youtubeRecommended() {
+	youtubeClear('.style-scope.ytd-compact-video-renderer', 'ytd-compact-video-renderer.style-scope:nth-child(', ') > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > h3:nth-child(1) > span:nth-child(2)', 'ytd-compact-video-renderer.style-scope:nth-child(', ') > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(2) > ytd-video-meta-block:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ytd-channel-name:nth-child(1) > div:nth-child(1) > div:nth-child(1) > yt-formatted-string:nth-child(1)', 'ytd-compact-video-renderer.style-scope:nth-child(', ') > div:nth-child(1)', "youtubeRecommended", 200);
+}
+
+function youtubeClearAll() {
+	console.log("youtubeClearAll called");
+	youtubeStartpage();
+	youtubeRecommended();
+}
+
+setTimeout(youtubeClearAll, 1000);
+
+var runningScroll = false;
+var oldScroll = $(document).scrollTop();
+$(document).on("scroll", function() {
+	var newScroll = $(document).scrollTop();
+	if (newScroll > oldScroll && ! runningScroll) {
+		oldScroll = newScroll;
+		runningScroll = true;
+		setTimeout(youtubeClearAll, 1000);
+		setTimeout(function() {
+			runningScroll = false;
+		}, 500);
+	}
+});
+
+var oldURL = document.URL;
+function urlChanged() {
+	var newURL = document.URL;
+	if (newURL !== oldURL) {
+		oldURL = newURL;
+		oldScroll = $(document).scrollTop();
+		count["youtubeStartpage"] = 1;
+		count["youtubeRecommended"] = 1;
+		setTimeout(youtubeClearAll, 1000);
+	}
+}
+
+setInterval(urlChanged, 1000);
+
+var listLength = {"youtubeStartpage": 0, "youtubeRecommended": 0};
+function checkLength() {
+	var youtubeStartpageLength = $('.style-scope.ytd-rich-grid-renderer').length;
+	var youtubeRecommendedLength = $('.style-scope.ytd-compact-video-renderer').length;
+	if (youtubeStartpageLength < listLength["youtubeStartpage"] || youtubeRecommendedLength < listLength["youtubeRecommended"]) {
+		count["youtubeStartpage"] = 1;
+		count["youtubeRecommended"] = 1;
+		setTimeout(youtubeClearAll, 1000);
+	}
+	listLength["youtubeStartpage"] = youtubeStartpageLength;
+	listLength["youtubeRecommended"] = youtubeRecommendedLength;
+}
+
+setInterval(checkLength, 1000);
+
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 1) {
+		getSearchTerms();
+	}
 });
